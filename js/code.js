555f354d56c2488ee83a2422450a3ca4569f6664
khadijah28 <kh929901@ucf.edu>
2026-04-07 23:15:53 -0400

    Add CI workflow with HTML, CSS, and JS linting
    - Add GitHub Actions workflow that runs ESLint, Stylelint, and HTMLHint on push and pull request
    - Add ESLint, Stylelint, and HTMLHint config files
    - Add .eslintignore to exclude third-party md5.js from linting
    - Fix alpha-value-notation in styles.css (0.31 -> 31%)
    - Add .gitignore to exclude node_modules

--- js/code.js
+++ js/code.js
@@ -5,102 +5,85 @@ let userId = 0;
 let firstName = "";
 let lastName = "";
 
-function doLogin()
-{
+// eslint-disable-next-line no-unused-vars
+function doLogin() {
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	
-	let login = document.getElementById("loginName").value;
-	let password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
-	
-	document.getElementById("loginResult").innerHTML = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
-	
-	let url = urlBase + '/Login.' + extension;
+	const login = document.getElementById("login-name").value;
+	const password = document.getElementById("login-password").value;
+	// const hash = md5( password );
 
-	let xhr = new XMLHttpRequest();
+	document.getElementById("login-result").innerHTML = "";
+
+	const tmp = { login: login, password: password };
+	// const tmp = {login:login,password:hash};
+	const jsonPayload = JSON.stringify(tmp);
+
+	const url = urlBase + '/Login.' + extension;
+
+	const xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
+	try {
+		xhr.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				const jsonObject = JSON.parse(xhr.responseText);
 				userId = jsonObject.id;
-		
-				if( userId < 1 )
-				{		
-					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+
+				if (userId < 1) {
+					document.getElementById("login-result").innerHTML = "User/Password combination incorrect";
 					return;
 				}
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 
 				saveCookie();
-	
+
 				window.location.href = "color.html";
 			}
 		};
 		xhr.send(jsonPayload);
+	} catch (err) {
+		document.getElementById("login-result").innerHTML = err.message;
 	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
-
 }
 
-function saveCookie()
-{
-	let minutes = 20;
-	let date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
+function saveCookie() {
+	const minutes = 20;
+	const date = new Date();
+	date.setTime(date.getTime() + (minutes * 60 * 1000));
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
-function readCookie()
-{
+// eslint-disable-next-line no-unused-vars
+function readCookie() {
 	userId = -1;
-	let data = document.cookie;
-	let splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
-		let thisOne = splits[i].trim();
-		let tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
+	const data = document.cookie;
+	const splits = data.split(",");
+	for (let i = 0; i < splits.length; i++) {
+		const thisOne = splits[i].trim();
+		const tokens = thisOne.split("=");
+		if (tokens[0] === "firstName") {
 			firstName = tokens[1];
-		}
-		else if( tokens[0] == "lastName" )
-		{
+		} else if (tokens[0] === "lastName") {
 			lastName = tokens[1];
-		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
+		} else if (tokens[0] === "userId") {
+			userId = parseInt(tokens[1].trim());
 		}
 	}
-	
-	if( userId < 0 )
-	{
+
+	if (userId < 0) {
 		window.location.href = "index.html";
-	}
-	else
-	{
-//		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	} else {
+		// document.getElementById("user-name").innerHTML = "Logged in as " + firstName + " " + lastName;
 	}
 }
 
-function doLogout()
-{
+// eslint-disable-next-line no-unused-vars
+function doLogout() {
 	userId = 0;
 	firstName = "";
 	lastName = "";
@@ -108,78 +91,64 @@ function doLogout()
 	window.location.href = "index.html";
 }
 
-function addColor()
-{
-	let newColor = document.getElementById("colorText").value;
-	document.getElementById("colorAddResult").innerHTML = "";
+// eslint-disable-next-line no-unused-vars
+function addColor() {
+	const newColor = document.getElementById("color-text").value;
+	document.getElementById("color-add-result").innerHTML = "";
 
-	let tmp = {color:newColor,userId,userId};
-	let jsonPayload = JSON.stringify( tmp );
+	const tmp = { color: newColor, userId: userId };
+	const jsonPayload = JSON.stringify(tmp);
 
-	let url = urlBase + '/AddColor.' + extension;
-	
-	let xhr = new XMLHttpRequest();
+	const url = urlBase + '/AddColor.' + extension;
+
+	const xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+	try {
+		xhr.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				document.getElementById("color-add-result").innerHTML = "Color has been added";
 			}
 		};
 		xhr.send(jsonPayload);
+	} catch (err) {
+		document.getElementById("color-add-result").innerHTML = err.message;
 	}
-	catch(err)
-	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
-	}
-	
 }
 
-function searchColor()
-{
-	let srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
-	
+// eslint-disable-next-line no-unused-vars
+function searchColor() {
+	const srch = document.getElementById("search-text").value;
+	document.getElementById("color-search-result").innerHTML = "";
+
 	let colorList = "";
 
-	let tmp = {search:srch,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
+	const tmp = { search: srch, userId: userId };
+	const jsonPayload = JSON.stringify(tmp);
 
-	let url = urlBase + '/SearchColors.' + extension;
-	
-	let xhr = new XMLHttpRequest();
+	const url = urlBase + '/SearchColors.' + extension;
+
+	const xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-				let jsonObject = JSON.parse( xhr.responseText );
-				
-				for( let i=0; i<jsonObject.results.length; i++ )
-				{
+	try {
+		xhr.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				document.getElementById("color-search-result").innerHTML = "Color(s) has been retrieved";
+				const jsonObject = JSON.parse(xhr.responseText);
+
+				for (let i = 0; i < jsonObject.results.length; i++) {
 					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
+					if (i < jsonObject.results.length - 1) {
 						colorList += "<br />\r\n";
 					}
 				}
-				
+
 				document.getElementsByTagName("p")[0].innerHTML = colorList;
 			}
 		};
 		xhr.send(jsonPayload);
+	} catch (err) {
+		document.getElementById("color-search-result").innerHTML = err.message;
 	}
-	catch(err)
-	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
-	}
-	
 }
